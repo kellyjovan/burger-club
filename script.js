121540e4d3511bb0249b8fc9a0ea0593f7591bbc
Jovan Kelly <jkelly@aoiths.org>
2017-06-29 14:49:36 +0000

    made changes

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 
-   
+  $('#signUp').click(function(){
+    $('#myModal').modal('show');
     
+    var name = $('#name').val();
+    var email = $('#email').val();
+    $('.modal-body').text("Hi, " + name + " thank you for signing up! We will send an email to " + email + "shortly");
+  });
+  $()
 
 });
